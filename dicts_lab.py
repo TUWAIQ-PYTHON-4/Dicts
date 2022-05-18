0f27753e26620ab14b6ca7f0ced0acd567745a44
BushraAlzahrani <102851258+BushraAlzahrani@users.noreply.github.com>
2022-05-18 16:09:30 +0300

    better solution for first question
    The output was printing not found until it finds the number the user gave as an input
    
    the output looked as the following:
    
    Sorry, the number is not found
    Sorry, the number is not found
    Sorry, the number is not found
    Sorry, the number is not found
    Rawan
    Sorry, the number is not found
    Sorry, the number is not found

--- dicts_lab.py
+++ dicts_lab.py
@@ -1,26 +1,6 @@
 
-'''
 ## Q1:Build a phone book program that receives the phone number, and returns the name of the owner. 
 You can follow the table below:
-
-| Name    | Number      |
-| -------- | ---------- |
-| Amal     | 0531511811 |
-| Mohammed | 0551511231 |
-| Khadijah | 0531511113 |
-| Abdullah  | 0541511123 |
-| Rawan    | 0591511128 |
-| Faisal   | 0581521129 |
-| Layla    | 0563513453 |
-
-
-- Ask the user to enter the phone number using input
-- If the number exists, print the owner. Otherwise, print "Sorry, the number is not found".
-- If the number is less or more than 10 numbers, print "This is invalid number".
-- If the number contains letters or symbols, print "This is invalid number".
-'''
-
-
 
 
 contacts= {'Amal': '0531511811' ,'Mohammed': '0551511231', 
@@ -30,19 +10,17 @@ contacts= {'Amal': '0531511811' ,'Mohammed': '0551511231',
 
 search_num = input('Enter phone number:')
 
-if len(search_num) < 10 or len(search_num) > 10:
-           print('This is invalid number')
-
-elif search_num.isalpha:
+if len(search_num) !=10:
     print('This is invalid number')
 
+elif not search_num.isnumeric:
+    print('This is invalid number')
 
-for name, phone in contacts.items():
-         if phone == search_num:
-             print(name)
+else:
+    print('Sorry, the number is not found') 
 
-         else:
-            print('Sorry, the number is not found') 
+name= [n for n, value in contacts.items() if value == search_num]
+print(name)
 
 
 ## Q2:Write a function that receives a list containing different numbers ex: [23, 0, 873, 0, 14, 50, 0], 
